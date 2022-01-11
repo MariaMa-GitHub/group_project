f891799965354d9544b06fa696fe8e5a5360a795
Maria <78451694+MariaMa-GitHub@users.noreply.github.com>
2022-01-11 14:24:48 -0500

    initial commit

---
+++ Library/Person.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Person here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.ArrayList;
+
+public class Person {
+    
+    String name;
+    short age;
+    int cardNum;
+    ArrayList<Media> possessions;
+    
+    public Person() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
