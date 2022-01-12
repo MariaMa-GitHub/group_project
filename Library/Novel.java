4c82a4b79f99e985408fe1597bfbac2fcdbec496
Bob Yang <87663549+bobyangg@users.noreply.github.com>
2022-01-12 13:17:33 -0500

    Create Novel.java

---
+++ Library/Novel.java
@@ -0,0 +1,24 @@
+/**
+ * Video games subclass
+ *
+ * @Bob Yang
+ * @Jan 11, 2022
+ */
+public class VideoGames extends Media
+{
+    String rating, compatability;
+    
+    public VideoGames(){
+        super();
+        this.rating = "TBD";
+        this.compatability = "N/A";
+    }
+    public VideoGames(String r, String c){
+        super(t, pub, g, a, hold);
+        this.rating = r;
+        this.compatability = c;
+    }
+    public String toString(){
+       return super().toString + String.format("Rating: %s\ncompatability: %s", this.rating, this.compatability);
+    }
+}
