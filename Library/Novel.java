e69edfacaee8ab00e62092d5bc350ce43a0060be
Bob Yang <87663549+bobyangg@users.noreply.github.com>
2022-01-14 13:07:51 -0500

    Add files via upload

--- Library/Novel.java
+++ Library/Novel.java
@@ -1,21 +1,31 @@
+/**
+ * Novel subclass
+ * Bob Yang
+ * Jan 14 2022
+ */
+
 public class Novel extends Media
 {
     String author;
     int pageCount;
     
+    //default constructor
     public Novel(){
         super();
         this.author = "TBD";
         this.pageCount = 0;
     }
+    //constructor that inherits from Media superclass
     public Novel(String t, String pub, String g, boolean a, Queue hold, String author, int p){
         super(t, pub, g, a, hold, 2);
         this.author = author;
         this.pageCount = p;
     }
+    //to string method
     public String toString(){
        return super.toString() + String.format("Author: %s\nPage count: %d", this.author, this.pageCount);
     }
+    //equals method
     public boolean equals(Novel v){
         if(this.author == v.author && this.pageCount == v.pageCount && this.publisher == v.publisher && this.genre == v.genre){
             return true;
--- Library/VideoGames.java
+++ Library/VideoGames.java
@@ -1,20 +1,30 @@
+/**
+ * Video games subclass
+ * Bob Yang
+ * Jan 14 2022
+ */
+
 public class VideoGames extends Media
 {
     String rating, compatibility;
     
+    //default constructor
     public VideoGames(){
         super();
         this.rating = "TBD";
         this.compatibility = "N/A";
     }
+    //constructor that inherits from Media superclass
     public VideoGames(String t, String pub, String g, boolean a, Queue hold, String r, String c){
         super(t, pub, g, a, hold, 3);
         this.rating = r;
         this.compatibility = c;
     }
+    //to string method
     public String toString(){
        return super.toString() + String.format("Rating: %s\ncompatibility: %s", this.rating, this.compatibility);
     }
+    //equals method
     public boolean equals(VideoGames v){
         if(this.rating == v.rating && this.compatibility == v.compatibility && this.publisher == v.publisher && this.genre == v.genre){
             return true;
