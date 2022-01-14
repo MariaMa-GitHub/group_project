667abfeedd7841e09bff25e8da3a59b25540141c
Bob Yang <87663549+bobyangg@users.noreply.github.com>
2022-01-14 14:09:34 -0500

    Update VideoGames.java

--- Library/VideoGames.java
+++ Library/VideoGames.java
@@ -6,23 +6,24 @@
 
 public class VideoGames extends Media
 {
-    String rating, compatibility;
+    String compatibility;
+    int rating;
     
     //default constructor
     public VideoGames(){
         super();
-        this.rating = "TBD";
+        this.rating = 0;
         this.compatibility = "N/A";
     }
     //constructor that inherits from Media superclass
-    public VideoGames(String t, String pub, String g, boolean a, Queue hold, String r, String c){
+    public VideoGames(String t, String pub, String g, boolean a, Queue hold, int r, String c){
         super(t, pub, g, a, hold, 3);
         this.rating = r;
         this.compatibility = c;
     }
     //to string method
     public String toString(){
-       return super.toString() + String.format("Rating: %s\nCompatibility: %s", this.rating, this.compatibility);
+       return super.toString() + String.format("Rating: %s\ncompatibility: %s", this.rating, this.compatibility);
     }
     //equals method
     public boolean equals(VideoGames v){
